94c5f798e19252e0cb2c550981423b9022fc26f0
Victor Felix <victorfgs@hotmail.com>
2018-06-13 05:48:37 -0300

    adding piuece component

---
+++ src/Piece/Piece.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+
+class Piece extends Component {
+    constructor(_color,
+                _type,
+                _selected,
+                _direction){
+        super();
+        this.color = _color
+        this.type = _type
+        this.selected = _selected
+        this.direction = _direction
+    }
+
+
+}
+
+
+
+export default Piece
